9dbc889631bcf6ca86edb3e23e06560d1475db41
Simon Sandström <simon@nikanor.nu>
2019-04-10 23:38:01 +0200

    client: add support for multiple servers, seems to work ¯\_(ツ)_/¯

--- src/pmp_client.cc
+++ src/pmp_client.cc
@@ -1,8 +1,10 @@
 #include <cstdio>
 #include <cmath>
+#include <deque>
 #include <string>
 #include <memory>
 #include <vector>
+#include <unordered_map>
 
 #include "tcp_backend.h"
 #include "protocol.h"
@@ -20,110 +22,147 @@ static struct Arguments
   int divisions;
 } arguments;
 
-static std::vector<Protocol::Request> requests;
-static Protocol::Request ongoing_request;
-static std::vector<std::uint8_t> ongoing_pixels;
-static std::vector<std::uint8_t> total_pixels;
+static std::deque<Protocol::Request> request_queue;
+static std::vector<std::uint8_t> image_pixels;
 
-static std::vector<std::tuple<std::string, std::string>> servers;
-static std::unique_ptr<TcpBackend::Connection> connection;
-
-static void send_request()
+struct Session
 {
+  std::unique_ptr<TcpBackend::Connection> connection;
+  Protocol::Request current_request;
+  std::vector<std::uint8_t> current_pixels;
+};
+static std::unordered_map<int, Session> sessions;
+
+static void send_request(int session_id)
+{
+  auto& session = sessions[session_id];
+
   // Fetch and remove next request from queue
-  ongoing_request = requests.front();
-  requests.erase(requests.begin());
+  session.current_request = request_queue.front();
+  request_queue.pop_front();
 
   // Make sure to clear ongoing_pixels
-  ongoing_pixels.clear();
+  session.current_pixels.clear();
 
   // And send it
-  connection->write(reinterpret_cast<const std::uint8_t*>(&ongoing_request), sizeof(ongoing_request));
+  session.connection->write(reinterpret_cast<const std::uint8_t*>(&session.current_request),
+                            sizeof(session.current_request));
 }
 
-static bool on_read(const std::uint8_t* buffer, int len)
+static bool on_read(int session_id, const std::uint8_t* buffer, int len)
 {
+  auto& session = sessions[session_id];
+
+  printf("%s: session_id=%d len=%d\n", __func__, session_id, len);
+
   Protocol::Response response;
   if (len != sizeof(response))
   {
     fprintf(stderr, "Unexpected data length (received=%d expected=%d)\n", len, static_cast<int>(sizeof(response)));
+
+    // Unrecoverable... for now
     exit(EXIT_FAILURE);
   }
-  else
+
+  std::copy(buffer, buffer + len, reinterpret_cast<std::uint8_t*>(&response));
+
+  printf("%s: response: num_pixels=%d last_message=%d\n",
+         __func__,
+         response.num_pixels,
+         response.last_message);
+
+  // Add the pixels we received
+  session.current_pixels.insert(session.current_pixels.end(), response.pixels, response.pixels + response.num_pixels);
+
+  if (response.last_message == 0)
   {
-    std::copy(buffer, buffer + len, reinterpret_cast<std::uint8_t*>(&response));
+    // Read more messages
+    return true;
+  }
 
-    printf("%s: response: num_pixels=%d last_message=%d\n",
-           __func__,
-           response.num_pixels,
-           response.last_message);
-
-    // Add the pixels we received
-    ongoing_pixels.insert(ongoing_pixels.end(), response.pixels, response.pixels + response.num_pixels);
-
-    if (response.last_message == 0)
-    {
-      // Read more messages
-      return true;
-    }
-
-    // Add ongoing_pixels to total_pixels, at the correct position
-    const auto sub_re = (arguments.max_c_re - arguments.min_c_re) / arguments.divisions;
-    const auto sub_im = (arguments.max_c_im - arguments.min_c_im) / arguments.divisions;
-    const auto dx = (ongoing_request.min_c_re - arguments.min_c_re) / sub_re;
-    const auto dy = (ongoing_request.min_c_im - arguments.min_c_im) / sub_im;
-    auto to = total_pixels.begin() + (ongoing_request.y * dy * arguments.x) + (ongoing_request.x * dx);
-    auto from = ongoing_pixels.begin();
-    for (auto y = 0; y < ongoing_request.y; y++)
-    {
-      std::copy(from, from + ongoing_request.x, to);
-      from += ongoing_request.x;
-      to += arguments.x;
-    }
+  // Add current_pixels to image_pixels, at the correct position
+  const auto sub_re = (arguments.max_c_re - arguments.min_c_re) / arguments.divisions;
+  const auto sub_im = (arguments.max_c_im - arguments.min_c_im) / arguments.divisions;
+  const auto dx = (session.current_request.min_c_re - arguments.min_c_re) / sub_re;
+  const auto dy = (session.current_request.min_c_im - arguments.min_c_im) / sub_im;
+  auto to = image_pixels.begin() + (session.current_request.y * dy * arguments.x) + (session.current_request.x * dx);
+  auto from = session.current_pixels.begin();
+  for (auto y = 0; y < session.current_request.y; y++)
+  {
+    std::copy(from, from + session.current_request.x, to);
+    from += session.current_request.x;
+    to += arguments.x;
   }
 
   // Check if there are more requests to handle
-  if (requests.empty())
+  if (!request_queue.empty())
   {
-    // TODO: this wont work when there are multiple connections/servers
-    printf("%s: no more requests, writing image\n", __func__);
-    PGM::write_pgm("test.pgm", arguments.x, arguments.y, total_pixels.data());
-
-    // Close connection
-    connection->close();
-    connection.reset();
-    return false;
+    // Handle next request and continue to read messages
+    send_request(session_id);
+    return true;
   }
 
-  // Handle next request and continue to read messages
-  send_request();
-  return true;
-}
+  // Close connection
+  sessions[session_id].connection->close();
+  sessions.erase(session_id);
 
-static void on_write()
-{
-  puts(__func__);
-}
-
-static void on_error(const std::string& message)
-{
-  puts(message.c_str());
-
-  if (connection)
+  // If this was the last session to be closed then we can write the image and we're done!
+  if (sessions.empty())
   {
-    connection->close();
+    printf("%s: no more requests and no more sessions, writing image\n", __func__);
+    PGM::write_pgm("test.pgm", arguments.x, arguments.y, image_pixels.data());
   }
+
+  return false;
 }
 
-static void on_connected(std::unique_ptr<TcpBackend::Connection>&& connection_)
+static void on_write(int session_id)
 {
-  puts(__func__);
+  printf("%s: session_id=%d\n", __func__, session_id);
+}
 
-  // Start TcpConnection
-  connection = std::move(connection_);
-  connection->start(on_read, on_write, on_error);
+static void on_error_connection(int session_id, const std::string& message)
+{
+  fprintf(stderr, "%s: session_id=%d message=%s\n", __func__, session_id, message.c_str());
+  sessions[session_id].connection->close();
+  sessions.erase(session_id);
+}
 
-  send_request();
+static void on_error_client(const std::string& message)
+{
+  fprintf(stderr, "%s: message=%s\n", __func__, message.c_str());
+}
+
+static void on_connected(std::unique_ptr<TcpBackend::Connection>&& connection)
+{
+  static int next_session_id = 0;
+  const auto session_id = next_session_id;
+  next_session_id += 1;
+
+  printf("%s: session_id=%d\n", __func__, session_id);
+
+  // Save new session
+  Session session;
+  session.connection = std::move(connection);
+  sessions.emplace(session_id, std::move(session));
+
+  // Create callbacks and start connection
+  auto read = [session_id](const std::uint8_t* buffer, int len)
+  {
+    return on_read(session_id, buffer, len);
+  };
+  auto write = [session_id]()
+  {
+    on_write(session_id);
+  };
+  auto error = [session_id](const std::string& message)
+  {
+    on_error_connection(session_id, message);
+  };
+  sessions[session_id].connection->start(read, write, error);
+
+  // Send next request in queue to this server
+  send_request(session_id);
 }
 
 int main(int argc, char* argv[])
@@ -168,7 +207,7 @@ int main(int argc, char* argv[])
   request.inf_n = arguments.max_n;
   request.x = sub_x;
   request.y = sub_y;
-  requests.push_back(request);
+  request_queue.push_back(request);
 
   // Add rest of the requests
   for (auto i = 1; i < std::pow(arguments.divisions, 2); i++)
@@ -183,10 +222,11 @@ int main(int argc, char* argv[])
       request.min_c_im += sub_im;
       request.max_c_im += sub_im;
     }
-    requests.push_back(request);
+    request_queue.push_back(request);
   }
 
   // Parse list of servers
+  std::vector<std::tuple<std::string, std::string>> servers;
   for (auto i = 9; i < argc; i++)
   {
     const auto arg = std::string(argv[i]);
@@ -203,18 +243,28 @@ int main(int argc, char* argv[])
   }
 
   // Pre-allocate total_pixels vector
-  total_pixels.insert(total_pixels.begin(), arguments.x * arguments.y, 0u);
+  image_pixels.insert(image_pixels.begin(), arguments.x * arguments.y, 0u);
 
-  // TODO: Use more than one server
-  const auto address = std::get<0>(servers[0]);
-  const auto port = std::get<1>(servers[0]);
-  printf("Connecting to address %s port %s\n", address.c_str(), port.c_str());
-
-  // Create TCP client
-  auto tcp_client = TcpBackend::create_client(address, port, on_connected, on_error);
-  if (!tcp_client)
+  // Create one TCP client per server
+  std::vector<std::unique_ptr<TcpBackend::Client>> clients;
+  for (const auto& server : servers)
   {
-    // Error message printed by TcpServer::create
+    const auto address = std::get<0>(server);
+    const auto port = std::get<1>(server);
+
+    printf("Creating TCP client to address %s port %s\n", address.c_str(), port.c_str());
+
+    // Create TCP client
+    auto tcp_client = TcpBackend::create_client(address, port, on_connected, on_error_client);
+    if (tcp_client)
+    {
+      clients.push_back(std::move(tcp_client));
+    }
+  }
+
+  if (clients.empty())
+  {
+    fprintf(stderr, "No TCP client could be created successfully\n");
     return EXIT_FAILURE;
   }
 
