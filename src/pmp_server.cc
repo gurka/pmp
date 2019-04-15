92508e2a9b0d7125ef163db39603cb7174f25409
Simon Sandström <simon@nikanor.nu>
2019-04-15 19:10:24 +0200

    server: merge connections & responses into Session

--- src/pmp_server.cc
+++ src/pmp_server.cc
@@ -14,114 +14,115 @@
 // The server
 static std::unique_ptr<TcpBackend::Server> server;
 
-// Map connection_id -> Connection
-static std::unordered_map<int, std::unique_ptr<TcpBackend::Connection>> connections;
+/**
+ * Represents a session
+ */
+struct Session
+{
+  std::unique_ptr<TcpBackend::Connection> connection;  /**< Pointer to Connection */
+  std::vector<std::uint8_t> pixels;                    /**< Array of pixels to be sent
+                                                            If Request not yet received or
+                                                            all Responses already sent it
+                                                            will be empty */
+};
 
-// Map connection_id -> Pixels
-static std::unordered_map<int, std::vector<std::uint8_t>> responses;
+// Map session_id -> Session
+static std::unordered_map<int, Session> sessions;
 
 /**
- * @brief Send (or continue to send) Response to given Connection
+ * @brief Send (or continue to send) Response to given Session
  *
  * Takes as many pixels as possible (Response message has a max size)
- * from responses and sends it to the Connection in a Response message.
+ * from responses and sends it to the Session in a Response message.
  *
- * @param[in]  connection_id  Id of the connection to which send Response
+ * @param[in]  session_id  Id of the session to which send Response
  */
-static void send_response(int connection_id)
+static void send_response(int session_id)
 {
-  if (responses.count(connection_id) == 0)
+  // Get Session and verify that we have pixels to send
+  auto& session = sessions.at(session_id);
+  if (session.pixels.empty())
   {
-    LOG_ERROR("%s: error: no pixels found for connection_id=%d", __func__, connection_id);
+    LOG_ERROR("%s: session_id=%d: no pixels to send", __func__, session_id);
     return;
   }
-
-  // Get pixels
-  auto& pixels = responses[connection_id];
 
   // Maximum size of byte array (pixels) in messages is 2^15 = 32768
   // so we need to split the response into multiple messages if we have
   // more pixels than that
   Protocol::Response response;
-  if (pixels.size() <= (1u << 15))
+  if (session.pixels.size() <= (1u << 15))
   {
     // We can send all pixels in this message
     // Serialize and send the message
-    response.pixels = responses[connection_id];
+    response.pixels = std::move(session.pixels);
     response.last_message = true;
     const auto buffer = Protocol::serialize(response);
-    connections[connection_id]->write(buffer.data(), buffer.size());
+    session.connection->write(buffer.data(), buffer.size());
 
-    // Erase the response/pixels
-    responses.erase(connection_id);
+    // Make sure that the Session's pixels vector is cleared
+    // (moving from it leaves it in an unspecified but defined state)
+    session.pixels.clear();
   }
   else
   {
-    // Send as many pixels as possible (2^15)
+    // Move as many pixels as possible (2^15) from Session's pixels vector
+    // to Response's pixels vector
     response.pixels.insert(response.pixels.end(),
-                           pixels.begin(),
-                           pixels.begin() + (1u << 15));
+                           session.pixels.begin(),
+                           session.pixels.begin() + (1u << 15));
+    session.pixels.erase(session.pixels.begin(), session.pixels.begin() + (1u << 15));
+
+    // Send the Response
     response.last_message = false;
     const auto buffer = Protocol::serialize(response);
-    connections[connection_id]->write(buffer.data(), buffer.size());
-
-    // Erase the pixels we sent from the vector
-    pixels.erase(pixels.begin(), pixels.begin() + (1u << 15));
+    session.connection->write(buffer.data(), buffer.size());
   }
 }
 
 /**
- * @brief Callback called when a connection disconnects
+ * @brief Callback called when a session disconnects
  *
- * Disconnect is expected only when the response has been sent,
- * if this is not the case we abort the program.
+ * Session is deleted. Server doesn't care if the disconnect
+ * was expected or not.
  *
- * @param[in]  connection_id  Id of the connection that disconnected
+ * @param[in]  session_id  Id of the session that disconnected
  */
-static void on_disconnected(int connection_id)
+static void on_disconnected(int session_id)
 {
-  LOG_INFO("Connection %d disconnected", connection_id);
-
-  // Delete the connection
-  connections.erase(connection_id);
-
-  // If the client disconnected unexpectedly (response still in queue) then
-  // we have to abort
-  if (responses.count(connection_id) == 1)
-  {
-    LOG_ERROR("%s: unexpected disconnect, response not sent", __func__);
-
-    // TODO: try to recover instead of just aborting
-    exit(EXIT_FAILURE);
-  }
+  LOG_INFO("Session %d disconnected", session_id);
+  sessions.erase(session_id);
 }
 
 /**
- * @brief Callback called when a connection has read a message
+ * @brief Callback called when a session has read a message
  *
  * The only message that is read is Request, so if we cannot deserialize
- * as a Request then abort.
+ * as a Request then we close the session.
  *
- * The Mandelbrot pixels are computated and added to the responses variable
- * and we then start sending a response to the connection.
+ * The Mandelbrot pixels are computated and added to session's pixels vector
+ * and we then start sending a response to the session.
  *
- * @param[in]  connection_id  Id of the connection that has read a message
- * @param[in]  buffer         Message data
- * @param[in]  len            Length of message data
+ * @param[in]  session_id  Id of the session that has read a message
+ * @param[in]  buffer      Message data
+ * @param[in]  len         Length of message data
  */
-static void on_read(int connection_id, const std::uint8_t* buffer, int len)
+static void on_read(int session_id, const std::uint8_t* buffer, int len)
 {
+  auto& session = sessions.at(session_id);
+
   Protocol::Request request;
   if (!Protocol::deserialize(std::vector<std::uint8_t>(buffer, buffer + len), &request))
   {
-    LOG_ERROR("%s: could not deseralize Request message", __func__);
-
-    // TODO: try to recover instead of just aborting
-    exit(EXIT_FAILURE);
+    LOG_ERROR("%s: session_id=%d: could not deseralize Request message, closing session",
+              __func__,
+              session_id);
+    session.connection->close();
+    return;
   }
 
-  LOG_INFO("Received request from connection %d: (%.2lf, %.2lf)..(%.2lf, %.2lf) (%d, %d) %d",
-           connection_id,
+  LOG_INFO("Received request from session %d: (%.2lf, %.2lf)..(%.2lf, %.2lf) (%d, %d) %d",
+           session_id,
            request.min_c.real(),
            request.min_c.imag(),
            request.max_c.real(),
@@ -138,90 +139,88 @@ static void on_read(int connection_id, const std::uint8_t* buffer, int len)
                                           request.max_iter);
   const auto time_end = std::chrono::steady_clock::now();
 
-  LOG_INFO("The request from connection %d took %dms (%ds) to compute",
-           connection_id,
+  LOG_INFO("The request from session %d took %dms (%ds) to compute",
+           session_id,
            std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_begin).count(),
            std::chrono::duration_cast<std::chrono::seconds>(time_end - time_begin).count());
 
-  // Add (move) pixels into the responses map
-  responses[connection_id] = std::move(pixels);
-
-  // Start sending response back to client
-  send_response(connection_id);
+  // Add (move) the pixels to the session object and start sending a response
+  session.pixels = std::move(pixels);
+  send_response(session_id);
 }
 
 /**
  * @brief Callback called when a session has written a message
  *
- * If the connection has more pixels to send we do that, otherwise
+ * If the session has more pixels to send we do that, otherwise
  * we re-start the read procedure to see if the client has more
  * requests.
  *
- * @param[in]  connection_id  Id of the connection that has written a message
+ * @param[in]  session_id  Id of the session that has written a message
  */
-static void on_write(int connection_id)
+static void on_write(int session_id)
 {
-  LOG_DEBUG("%s: connection_id=%d", __func__, connection_id);
+  LOG_DEBUG("%s: session_id=%d", __func__, session_id);
 
   // Check if there are more pixels to send
-  if (responses.count(connection_id) == 1)
+  auto& session = sessions.at(session_id);
+  if (!session.pixels.empty())
   {
-    send_response(connection_id);
+    send_response(session_id);
   }
   else
   {
-    LOG_INFO("Response successfully sent to connection %d", connection_id);
+    LOG_INFO("Response successfully sent to session %d", session_id);
 
     // Restart read procedure to see if the client has more requests
-    connections[connection_id]->read();
+    session.connection->read();
   }
 }
 
 /**
- * @brief Callback called when an error occurs in a connection
+ * @brief Callback called when an error occurs in a session
  *
- * Errors are currently not handled and the program is aborted on any
- * kind of error.
+ * The session is closed on any type of error.
  *
- * @param[in]  connection_id  Id of the connection for which an error occurred
- * @param[in]  message        Error message
+ * @param[in]  session_id  Id of the session for which an error occurred
+ * @param[in]  message     Error message
  */
-static void on_error(int connection_id, const std::string& message)
+static void on_error(int session_id, const std::string& message)
 {
-  LOG_ERROR("%s: connection_id=%d message=%s", __func__, connection_id, message.c_str());
-
-  // TODO: try to recover instead of just aborting
-  exit(EXIT_FAILURE);
+  LOG_ERROR("%s: session_id=%d message=%s", __func__, session_id, message.c_str());
+  auto& session = sessions.at(session_id);
+  session.connection->close();
 }
 
 /**
  * @brief Callback called when the Server has accepted a connection
  *
- * The connection is initialized and the read procedure is started.
+ * A Session is created and the read procedure is started.
  * We also tell the server to continue accepting new connections.
  *
  * @param[in]  connection  The connection, wrapped in std::unique_ptr
  */
 static void on_accept(std::unique_ptr<TcpBackend::Connection>&& connection)
 {
-  // Unique id per connection
-  static int next_connection_id = 0;
-  const auto connection_id = next_connection_id++;
+  // Unique id per session / connection
+  static int next_session_id = 0;
+  const auto session_id = next_session_id++;
 
-  LOG_INFO("Connection %d connected", connection_id);
+  LOG_INFO("Session %d connected", session_id);
 
-  // Save connection
-  connections[connection_id] = std::move(connection);
+  // Create Session
+  auto& session = sessions[session_id];
+  session.connection = std::move(connection);
 
   // Set callbacks
-  const auto disconnected  = [connection_id]()                                    { on_disconnected(connection_id);      };
-  const auto read          = [connection_id](const std::uint8_t* buffer, int len) { on_read(connection_id, buffer, len); };
-  const auto write         = [connection_id]()                                    { on_write(connection_id);             };
-  const auto error         = [connection_id](const std::string& message)          { on_error(connection_id, message);    };
-  connections[connection_id]->set_callbacks(disconnected, read, write, error);
+  const auto disconnected  = [session_id]()                                    { on_disconnected(session_id);      };
+  const auto read          = [session_id](const std::uint8_t* buffer, int len) { on_read(session_id, buffer, len); };
+  const auto write         = [session_id]()                                    { on_write(session_id);             };
+  const auto error         = [session_id](const std::string& message)          { on_error(session_id, message);    };
+  session.connection->set_callbacks(disconnected, read, write, error);
 
   // Start read procedure
-  connections[connection_id]->read();
+  session.connection->read();
 
   // Continue to accept new connections
   server->accept();
